657d678403f87baeaaa933b66a0cdd964bff8543
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-09-20 19:35:03 -0300

    feat: add test validations

--- tests/integration/inventory.test.js
+++ tests/integration/inventory.test.js
@@ -1,21 +1,38 @@
+/* eslint-disable max-lines-per-function */
 const request = require('supertest');
 const app = require('../../src/app');
 const { inventory1, patchInventory } = require('./mocks');
 
 describe('Inventory test', () => {
-  it('GET /produtos/:id/estoque', async () => {
-    const res = await request(app).get('/produtos/1/estoque');
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual(inventory1);
+  describe('GET /produtos/:id/estoque', () => {
+    it('ok', async () => {
+      const res = await request(app).get('/produtos/1/estoque');
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(inventory1);
+    });
+
+    it('not found error', async () => {
+      const res = await request(app).get('/produtos/10/estoque');
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ message: 'Not found.' });
+    });
   });
 
-  it('PATCH /produtos/:id/estoque', async () => {
-    const res = await request(app).patch('/produtos/3/estoque').send(patchInventory);
-    expect(res.statusCode).toBe(204);
+  describe('PATCH /produtos/:id/estoque', () => {
+    it('ok', async () => {
+      const res = await request(app).patch('/produtos/1/estoque').send(patchInventory);
+      expect(res.statusCode).toBe(204);
+    });
+
+    it('not found error', async () => {
+      const res = await request(app).patch('/produtos/10/estoque').send(patchInventory);
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ message: 'Not found.' });
+    });
   });
 
   it('DELETE /produtos/:id/estoque', async () => {
-    const res = await request(app).delete('/produtos/3/estoque');
+    const res = await request(app).delete('/produtos/1/estoque');
     expect(res.statusCode).toBe(501);
   });
 });
