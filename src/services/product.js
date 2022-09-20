23f465f0c179e114e3fe91f2888d5e401aaf68fd
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-09-20 20:14:08 -0300

    feat: add product-inventory transaction

--- src/services/product.js
+++ src/services/product.js
@@ -1,4 +1,8 @@
-const { Product } = require('../models');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+const { Product, Inventory } = require('../models');
+
+const sequelize = new Sequelize(config[process.env.NODE_ENV]);
 
 const getAll = async () => {
   const products = await Product.findAll();
@@ -11,15 +15,26 @@ const getById = async (id) => {
 };
 
 const create = async (productInfo) => {
-  const product = await Product.create({ ...productInfo });
-  return product;
+  const t = await sequelize.transaction();
+  try {
+    const product = await Product.create(productInfo, { transaction: t });
+    const inventory = {
+      idProduto: product.id,
+      quantidade: 0,
+      reserva: 0,
+      status: productInfo.status,
+    };
+    await Inventory.create(inventory, { transaction: t });
+    await t.commit();
+    return product;
+  } catch (e) {
+    await t.rollback();
+    throw e;
+  }
 };
 
 const update = async (id, productInfo) => {
-  await Product.update(
-    { ...productInfo },
-    { where: { id } },
-  );
+  await Product.update(productInfo, { where: { id } });
 };
 
 const remove = async (id) => {
