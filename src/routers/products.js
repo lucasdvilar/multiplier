ae45ac0787b45651567657fab45a4264ecf15317
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-09-20 17:54:06 -0300

    feat: add inventory routes

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const ProductController = require('../controllers/product');
+const InventoryController = require('../controllers/inventory');
 
 const router = Router();
 
@@ -8,5 +9,9 @@ router.get('/:id', ProductController.getById);
 router.post('/', ProductController.create);
 router.patch('/:id', ProductController.update);
 router.delete('/:id', ProductController.remove);
+
+router.get('/:id/estoque', InventoryController.getById);
+router.patch('/:id/estoque', InventoryController.update);
+router.delete('/:id/estoque', InventoryController.remove);
 
 module.exports = router;
