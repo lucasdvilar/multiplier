90d5483cb07337426f7b84564612c7218573873c
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-09-20 21:22:44 -0300

    feat: add product validation tests

--- tests/integration/products.test.js
+++ tests/integration/products.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-lines-per-function */
 const request = require('supertest');
 const app = require('../../src/app');
-const { products, postProduct, postProductWithId, patchProduct } = require('./mocks');
+const { products, postProduct, postProductWithId, patchProduct, inventory3 } = require('./mocks');
 
 describe('Products test', () => {
   it('GET /produtos', async () => {
@@ -10,25 +10,51 @@ describe('Products test', () => {
     expect(res.body).toEqual(products);
   });
 
-  it('GET /produtos/:id', async () => {
-    const res = await request(app).get('/produtos/1');
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual(products[0]);
+  describe('GET /produtos/:id', () => {
+    it('ok', async () => {
+      const res = await request(app).get('/produtos/1');
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(products[0]);
+    });
+
+    it('not found error', async () => {
+      const res = await request(app).get('/produtos/10');
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ message: 'Not found.' });
+    });
   });
 
   it('POST /produtos', async () => {
     const res = await request(app).post('/produtos').send(postProduct);
+    const inventoryRes = await request(app).get('/produtos/3/estoque');
     expect(res.statusCode).toBe(201);
     expect(res.body).toEqual(postProductWithId);
+    expect(inventoryRes.body).toEqual(inventory3);
   });
 
-  it('PATCH /produtos/:id', async () => {
-    const res = await request(app).patch('/produtos/3').send(patchProduct);
-    expect(res.statusCode).toBe(204);
+  describe('PATCH /produtos/:id', () => {
+    it('ok', async () => {
+      const res = await request(app).patch('/produtos/3').send(patchProduct);
+      expect(res.statusCode).toBe(204);
+    });
+
+    it('not found error', async () => {
+      const res = await request(app).patch('/produtos/10').send(patchProduct);
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ message: 'Not found.' });
+    });
   });
 
-  it('DELETE /produtos/:id', async () => {
-    const res = await request(app).delete('/produtos/3');
-    expect(res.statusCode).toBe(204);
+  describe('DELETE /produtos/:id', () => {
+    it('ok', async () => {
+      const res = await request(app).delete('/produtos/3');
+      expect(res.statusCode).toBe(204);
+    });
+
+    it('not found error', async () => {
+      const res = await request(app).delete('/produtos/10');
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ message: 'Not found.' });
+    });
   });
 });
